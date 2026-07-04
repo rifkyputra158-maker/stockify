de2221609d46f7501d799239e445b1f038ae42fa
rifkyputra158-maker <rifkyputra158@gmail.com>
2026-07-04 14:40:06 +0700

    chore: initial laravel setup

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::name('index-practice')->get('/', function () {
+    return view('pages.practice.index');
+});
+
+Route::name('practice.')->group(function () {
+    Route::name('first')->get('practice/1', function () {
+        return view('pages.practice.1');
+    });
+    Route::name('second')->get('practice/2', function () {
+        return view('pages.practice.2');
+    });
+});
